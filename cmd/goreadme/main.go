383aacb8a1b29a882273d23a1e133367fa8878eb
Eyal Posener <posener@gmail.com>
2019-02-23 23:36:59 +0200

    cleanups

--- cmd/goreadme/main.go
+++ cmd/goreadme/main.go
@@ -5,23 +5,30 @@ package main
 import (
 	"context"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/posener/goreadme/goreadme"
 	"golang.org/x/oauth2"
 )
 
+var githubToken = os.Getenv("GITHUB_TOKEN")
+
 func main() {
+	ctx := context.Background()
 	if len(os.Args) < 2 {
 		log.Fatal("Missing argument repository name")
 	}
-	ctx := context.Background()
 
-	client := oauth2.NewClient(ctx, oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")},
-	))
+	client := http.DefaultClient
+	if githubToken != "" {
+		client = oauth2.NewClient(ctx, oauth2.StaticTokenSource(
+			&oauth2.Token{AccessToken: githubToken},
+		))
+	}
 
-	err := goreadme.Create(ctx, client, os.Args[1], os.Stdout)
+	g := goreadme.GoReadme{Client: client}
+	err := g.Create(ctx, os.Args[1], os.Stdout)
 	if err != nil {
 		log.Fatalf("Failed: %s", err)
 	}

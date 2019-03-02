d4b9c21aecbedd096819c3c8bd01fafec2fea00c
Eyal Posener <posener@gmail.com>
2019-03-02 11:31:07 +0200

    add github badges support

--- template.go
+++ template.go
@@ -1,12 +1,33 @@
 package goreadme
 
-import "text/template"
+import (
+	"strings"
+	"text/template"
+
+	"github.com/golang/gddo/doc"
+)
 
 var tmpl = template.Must(template.New("readme").Funcs(
 	template.FuncMap{
-		"code": func(s string) string { return "```golang\n" + s + "\n```\n" },
+		"code": func(s string) string {
+			return "```golang\n" + s + "\n```\n"
+		},
+		"fullName": func(p *doc.Package) string {
+			return strings.TrimPrefix(p.ImportPath, "github.com/")
+		},
 	},
 ).Parse(`# {{.Package.Name}}
+
+{{if .Config.Badges.TravicCI}}
+[![Build Status](https://travis-ci.org/{{fullName .Package}}.svg?branch=master)](https://travis-ci.org/{{fullName .Package}}){{end -}}
+{{if .Config.Badges.CodeCov}}
+[![codecov](https://codecov.io/gh/{{fullName .Package}}/branch/master/graph/badge.svg)](https://codecov.io/gh/{{fullName .Package}}){{end -}}
+{{if .Config.Badges.GolangCI}}
+[![golangci](https://golangci.com/badges/{{.Package.ImportPath}}.svg)](https://golangci.com/r/{{.Package.ImportPath}}){{end -}}
+{{if .Config.Badges.GoDoc}}
+[![GoDoc](https://godoc.org/{{.Package.ImportPath}}?status.svg)](http://godoc.org/{{.Package.ImportPath}}){{end -}}
+{{if .Config.Badges.GoReportCard}}
+[![Go Report Card](https://goreportcard.com/badge/{{.Package.ImportPath}})](https://goreportcard.com/report/{{.Package.ImportPath}}){{end -}}
 
 {{ .Package.Doc -}}
 {{if (and .SubPackages (not .Config.SkipSubPackages)) }}

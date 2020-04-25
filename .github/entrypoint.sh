a411b651311e457e7989eef04911f2f1eef07531
Eyal Posener <posener@gmail.com>
2020-04-25 12:07:19 +0300

    Update goreadme comments

--- .github/entrypoint.sh
+++ .github/entrypoint.sh
@@ -8,20 +8,39 @@ IMPORT_PATH="${INPUT_IMPORT_PATH:-github.com/${GITHUB_REPOSITORY}}"
 BRANCH=$(echo "${GITHUB_REF}" | cut -d/ -f3)
 EMAIL="${INPUT_COMMIT-EMAIL:-posener@gmail.com}"
 
+REVIEWS_URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls/${BRANCH}/reviews"
+COMMENT_HEADER="[Goreadme](https://github.com/posener/goreadme) diff for \`${README_FILE}\` file for this PR:"
+
 echo "Processing: ${IMPORT_PATH}@${BRANCH}
 Event: ${GITHUB_EVENT_NAME}
 "
 
+get_existing_comment_id() {
+    CURRENT_REVIEWS="$(curl -sS "${REVIEWS_URL}")"
+    LINE="$(echo "${CURRENT_REVIEWS}" | jq ".[] | .body" | grep -Fn "${COMMENT_HEADER}" | cut -d: -f1)"
+    if [[ -n "${LINE}" ]]
+    then
+        echo "${CURRENT_REVIEWS}" | jq ".["$(( LINE - 1 ))"] | .id"
+    fi
+}
+
 # Run Goreadme on the current HEAD.
-goreadme -import-path="${IMPORT_PATH}" $@ > ${README_FILE}
+goreadme -import-path="${IMPORT_PATH}" "$@" > "${README_FILE}"
 
 # Check if README was modified or was added, and don't push changes if nothing changed.
-git add ${README_FILE}
-if git diff --staged --exit-code --no-color ${README_FILE} > readme_diff.txt
+git add "${README_FILE}"
+if git diff --staged --exit-code --no-color "${README_FILE}" > readme_diff.txt
 then
     echo "No changes were made to ${README_FILE}, aborting"
     exit 0
 fi
+
+# Diff content.
+DIFF=$(cat readme_diff.txt | tail +5 | sed "s/\`/'/g")
+echo "Diff:
+
+${DIFF}
+"
 
 if [[ "${GITHUB_EVENT_NAME}" == "push" ]]
 then
@@ -33,46 +52,56 @@ then
 
     # Commit and push chnages to upstream branch.
     git commit -m "Update readme according to Go doc"
-    git push origin HEAD:${BRANCH}
+    git push origin HEAD:"${BRANCH}"
 else
     echo "Pull request mode"
+    # Add comment on PR if Github token was given.
+    if [[ -z "${GITHUB_TOKEN}" ]]
+    then
+        echo "In order to add request comment, set the github_token input."
+        exit 0
+    fi
 
     # Prepare comment text.
-    BODY=$(cat readme_diff.txt | tail +5 | sed "s/\`/'/g")
 
-    echo "Diff:\n\n${BODY}"
-
-    BODY="
-[Goreadme](https://github.com/posener/goreadme) diff for \`${README_FILE}\` file for this PR:
+    COMMENT_TEXT="${COMMENT_HEADER}
 
 \`\`\`diff
-${BODY}
+${DIFF}
 \`\`\`
 
 This change will be automatically pushed when this PR is merged.
 "
-    
-    # Add comment on PR if Github token was given.
-    if [[ -z "${GITHUB_TOKEN}" ]]
+
+    # Prepare the comment text for json:
+    COMMENT_TEXT=$(echo "${COMMENT_TEXT}" | sed 's/\"/\\"/g' | sed ':a;N;$!ba;s/\n/\\n/g')
+    ACTION="POST"
+
+    # Check if there is already a review that contains goreadme contnet
+    COMMENT_ID="$(get_existing_comment_id)"
+    if [[ -z "${COMMENT_ID}" ]]
     then
-        echo "
-In order to add request comment, set the github_token input.
-${BODY}
-"
-        exit 0
+        echo "Creating new comment"
+        CURL_BODY="{\"body\": \"${COMMENT_TEXT}\", \"event\": \"COMMENT\"}"
+    else
+        echo "Updating comment with ID: ${COMMENT_ID}"
+        ACTION="PUT"
+        CURL_BODY="{\"body\": \"${COMMENT_TEXT}\"}"
     fi
-
-    # Make the API call to post the comment.
-
-    # Prepare the body for json:
-    BODY=$(echo "${BODY}" | sed 's/\"/\\"/g' | sed ':a;N;$!ba;s/\n/\\n/g')
     
-    curl "https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls/${BRANCH}/reviews" \
-        --fail \
+    # Perform comment post/update request.
+    if ! curl -X "${ACTION}" "${REVIEWS_URL}/${COMMENT_ID}" \
+        --fail -sS \
         -H "Content-Type: application/json" \
         -H "Authorization: token ${GITHUB_TOKEN}" \
-        -d "{
-            \"event\": \"COMMENT\",
-            \"body\": \"${BODY}\"
-           }"
+        -d "${CURL_BODY}"
+    then
+        echo "Failed updating comment.
+
+URL:    ${REVIEWS_URL}/${COMMENT_ID}
+Action: ${ACTION}
+Body:   ${CURL_BODY}
+"
+        exit 1
+    fi
 fi

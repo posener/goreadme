45d2c8cd1389c97fa86dfa4cd5bd79618de1d638
Chris Fenner <kettletoast@gmail.com>
2021-05-18 19:12:21 -0700

    Add test cases for factory functions, methods, consts and vars

--- testdata/pkg1/pkg1.go
+++ testdata/pkg1/pkg1.go
@@ -64,6 +64,22 @@ package pkg1
 
 import "fmt"
 
+const (
+	// ConstVal1 is a const in a const block.
+	ConstVal1 int = 1
+)
+
+// ConstVal2 is a const outside a const block.
+const ConstVal2 string = "2"
+
+var (
+	// VarVal1 is a var in a var block.
+	VarVal1 int = 3
+)
+
+// VarVal2 is a var outside a var block.
+var VarVal2 string = "4"
+
 func Func() {
 	fmt.Println("hello")
 }

0195c21c84257ed1fbba492f0f21bc884e82777c
eahrend <kire317@gmail.com>
2021-07-06 15:41:05 -0400

    updating goreadme.go

--- goreadme.go
+++ goreadme.go
@@ -16,9 +16,12 @@
 //
 //  on:
 //    push:
-//      branches: [master]
+//      branches: [main]
 //    pull_request:
-//      branches: [master]
+//      branches: [main]
+//  permissions:
+//    # Goreadme needs permissions to update pull requests comments.
+//    pull-requests: write
 //  jobs:
 //      goreadme:
 //          runs-on: ubuntu-latest
